12d80ad929adfb5f0083390083379d59222d8f6a
ICheered <tjeerd992@gmail.com>
2025-01-13 09:59:14 +0100

    Add setup_dotfiles script

---
+++ setup_dotfiles.sh
@@ -0,0 +1,5 @@
+# bash
+ln -sf ~/Dotfiles/.zshrc ~/.zshrc
+ln -sf ~/Dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/Dotfiles/.vimrc ~/.vimrc
+ln -sf ~/Dotfiles/.xbindkeysrc ~/.xbindkeysrc
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,4 +1,5 @@
 export PATH="/home/tjbakker/.local/bin:$PATH"
+export PATH="$HOME/.cargo/bin:$PATH"
 
 # export NVM_DIR="$HOME/.nvm"
 # [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
